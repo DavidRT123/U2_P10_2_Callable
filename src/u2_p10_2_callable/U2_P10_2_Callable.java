bf93f8fd6884f368497a49618608df901005003c
David <mdfdavid123@gmail.com>
2018-10-12 16:23:42 +0200

    Dos putos días de trabajo pero al final lo he conseguido!!!!!

---
+++ src/u2_p10_2_callable/U2_P10_2_Callable.java
@@ -0,0 +1,92 @@
+/*
+ * Crea un nuevo programa, llámalo U2_P10_2_Callable basado en el anterior para ejecutar el
+ * procedimiento y la función (p_nombredep y f_nombredep respectivamente)
+ */
+package u2_p10_2_callable;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Types;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author mdfda
+ */
+public class U2_P10_2_Callable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int resp = 0;
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Introduce departamento: ");
+        resp = sc.nextInt();
+        
+        try {
+            Class.forName("org.hsqldb.jdbc.JDBCDriver");
+
+            Connection con = DriverManager.getConnection("jdbc:hsqldb:C://hsqldb-2.4.1//hsqldb//hsqldb//ejemplo1", "SA", "");
+
+            CallableStatement cS = con.prepareCall("CALL p_nombredep(?, ?)");
+            
+            cS.setInt(1, resp);
+            cS.registerOutParameter(2, Types.VARCHAR); //Se le pasa parámetro de salida para que podamos acceder al resultado del procedimiento
+            
+            //Ejecutar procedimiento
+            cS.executeUpdate();
+            
+            System.out.println("***** PROCEDIMIENTO *****");
+            System.out.print("El departamento nº: " + resp);
+            
+            if(cS.getString(2) == null){
+                System.out.println(" NO EXISTE");
+            }else{
+                System.out.println(" se llama: " + cS.getString(2));
+            }
+            System.out.println("");
+            
+            ////////////////////////////////////////////////////////////////////////////////
+            
+            System.out.println("***** FUNCIÓN *****");
+            
+            cS = con.prepareCall("CALL f_nombredep(?)");
+            
+            cS.setInt(1, resp);
+            
+            //Ejecutar función
+            ResultSet r = cS.executeQuery();
+            
+            //Situamos el resultado sobre la primera fila
+            r.next();
+            
+            System.out.print("El departamento nº: " + resp);
+            
+            if(r.getString(1) == null){
+                System.out.println(" NO EXISTE");
+            }else{
+                System.out.println(" se llama: " + r.getString(1));
+            }
+            System.out.println("");
+                    
+            cS.close();
+            con.close();
+
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(U2_P10_2_Callable.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            System.out.println("Código de error: " + ex.getErrorCode());
+            System.out.println("Mensaje: " + ex.getMessage());
+            System.out.println("Estado SQL: " + ex.getSQLState());
+        }
+    }
+    
+}
